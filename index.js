fd687fd419120367f90ed415f3dc677e9a68c643
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-08 17:53:18 -0500

    starting buildTeam fcn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// index.js that runs team-generator app, TWH, 7/8/21
+// index.js; runs team-generator app, TWH, 7/8/21
 
 const inquirer = require("inquirer");
+const Team = require("./lib/Team");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -8,63 +9,107 @@ const Intern = require("./lib/Intern");
 const generate = require('./src/generateHTML');
 const fs = require('fs');
 
-start();
+// global variable will hold the team itself
+let myTeam = new Team("My new dev team");
 
+// launches app
 const start = () => {
     inquirer.prompt([
-        {type: "input", message: "Enter in team manager's name:", name: "managerName"},
-        {type: "input", message: "Enter in team manager's employee ID:", name: "managerID"},
-        {type: "input", message: "Enter in team manager's email address:", name:"managerEmail"},
-        {type: "input", message: "Enter in office number:", name: "officeNumber"},
-      ]).then( data => {
-        
-        console.log(data)
-        
+        { type: "input", message: "Enter in team manager's name:", name: "managerName" },
+        { type: "input", message: "Enter in team manager's employee ID:", name: "managerID" },
+        { type: "input", message: "Enter in team manager's email address:", name: "managerEmail" },
+        { type: "input", message: "Enter in office number:", name: "officeNumber" },
+
+    ]).then( ({name, id, email, officeNumber}) => {
+        // instantiate new manager
+        const addedManager = new Manager(name, id, email, officeNumber)
+    
+        // add manager to team
+        myTeam.addToTeam(addedManager);
+
         createTeam()
     })
 }
 
-// begins createTeam (after manager input)
+start();
+
+// begins createTeam (after manager finishes their input)
 const createTeam = () => {
     inquirer.prompt([
-      {
-        type: "list",
-        message: "Choose an option:",
-        choices: [ "Add Engineer", "Add Intern", "Finish & Build Team"],
-        name: "createTeam"
-      }
-    ]).then( data => {
-      switch( data.createTeam ){
-        case "Add Engineer":
-          addEngineer();
-          break;
-  
-        case "Add Intern":
-          addIntern();
-          break;
-  
-        case "Finish & Build Team":
-          buildTeam();
-          break;
-      }
+        {
+            type: "list",
+            message: "Choose an option:",
+            choices: ["Add Engineer", "Add Intern", "Finish & Build Team"],
+            name: "createTeam"
+        }
+    ]).then(data => {
+        switch (data.createTeam) {
+            case "Add Engineer":
+                addEngineer();
+                break;
+
+            case "Add Intern":
+                addIntern();
+                break;
+
+            case "Finish & Build Team":
+                buildTeam();
+                break;
+        }
     })
+}
+
+// displays prompts for engineer
+const addEngineer = () => {
+    inquirer.prompt([
+      { type: "input", message: "What's the engineer's name?", name: "name" },
+      { type: "input", message: "What's their ID?", name: "id" },
+      { type: "input", message: "What's their email?", name: "email" },
+      { type: "input", message: "What's their GitHub username?", name: "gitHub" },
+      
+    ]).then( ({name, id, email, gitHub}) => {
+      // instantiate a new engineer
+      const addedEngineer = new Engineer(name, id, email, gitHub)
+  
+      // adds the engineer to the team
+      myTeam.addToTeam(addedEngineer);
+  
+      // goes back to createTeam
+      createTeam();
+    });
   }
 
+  // displays prompts for adding intern
+const addIntern = () => {
+    inquirer.prompt([
+      { type: "input", message: "What's the intern's name?", name: "name" },
+      { type: "input", message: "What's their ID?", name: "id" },
+      { type: "input", message: "What's their email?", name: "email" },
+      { type: "input", message: "What's their current school?", name: "school" },
+      
+    ]).then( ({name, id, email, school}) => {
+      // instantiate a new intern
+      const addedIntern = new Intern(name, id, email, school)
   
+      // add the intern to the team
+      myTeam.addToTeam(addedIntern);
+  
+      // gooes back to createTeam
+      createTeam();
+    });
+  }
 
+  //this invokes the src write file and passes in myTeam array of objects data?
+  const buildTeam = () => {
 
-
-
-
-
-
+  }
 
 
 // initializes file
 // function startTeamFile(data) {
- 
+
 //     const fileName = `${data.managerName.toLowerCase()}.html`
-    
+
 //     writeTeamFile(fileName, data)
 // }
 
@@ -75,4 +120,3 @@ const createTeam = () => {
 //         if(err) console.log("err:", err);
 //     })
 // }
-
