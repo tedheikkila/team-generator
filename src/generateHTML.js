0ce6d9844da17d152785c1369e344a2899423bfb
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-10 12:39:24 -0500

    updated template literal

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -7,15 +7,17 @@ function renderEngineers(myTeam) {
 
   for (let i = 0; i < filtered.length; i++) {
     let engineerCard = `
-    <div class="card">
-      <div class="card-body">
-        <h4 class="engineer-title">${filtered[i].name}</h4>
-        <p class ="engineer-icon">${filtered[i].position}</p>
-        <p class="card-text">ID: <span class="engineer-id">${filtered[i].id}</span></p>
-        <p class="card-text"><span class="engineer-email">Email: </span><a href="mailto:${filtered[i].email}">${filtered[i].email}</a></p>
-        <span class="card-text">GitHub: </span><a class ="engineer-github" href="https://github.com/${filtered[i].gitHub}" target="_blank">${filtered[i].gitHub}</a></span></p>
-      </div>
-    </div> 
+     <div class="col-12 col-md-6 col-lg-3">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="engineer-title">${filtered[i].name}</h4>
+          <p class ="engineer-icon">${filtered[i].position}</p>
+          <p class="card-text">ID: <span class="engineer-id">${filtered[i].id}</span></p>
+          <p class="card-text"><span class="engineer-email">Email: </span><a href="mailto:${filtered[i].email}">${filtered[i].email}</a></p>
+          <span class="card-text">GitHub: </span><a class ="engineer-github" href="https://github.com/${filtered[i].gitHub}" target="_blank">${filtered[i].gitHub}</a></span></p>
+        </div>
+      </div> 
+    </div>
     `
     engineerCards += engineerCard
   }
@@ -31,15 +33,17 @@ function renderInterns(myTeam) {
 
   for (let i = 0; i < filtered.length; i++) {
     let internCard = `
-    <div class="card">
-      <div class="card-body">
-        <h4 class="intern-title">${filtered[i].name}</h4>
-        <p class ="intern-icon">${filtered[i].position}</p>
-        <p class="card-text">ID: <span class="intern-id">${filtered[i].id}</span></p>
-        <p class="card-text"><span class="intern-email">Email: </span><a href="mailto:${filtered[i].email}">${filtered[i].email}</a></p>
-        <p class="card-text">School: <span class ="intern-school"></span>${filtered[i].school}</p>
+    <div class="col-12 col-md-6 col-lg-3">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="intern-title">${filtered[i].name}</h4>
+          <p class ="intern-icon">${filtered[i].position}</p>
+          <p class="card-text">ID: <span class="intern-id">${filtered[i].id}</span></p>
+          <p class="card-text"><span class="intern-email">Email: </span><a href="mailto:${filtered[i].email}">${filtered[i].email}</a></p>
+          <p class="card-text">School: <span class ="intern-school"></span>${filtered[i].school}</p>
         </div>
-    </div> 
+      </div> 
+    </div>
     `
     internCards += internCard
   }
@@ -77,22 +81,26 @@ function generateHTML(myTeam) {
 
     <!--members (manager, engineers, interns)-->
     <div class="card-deck">
-      <div class="card">
-        <div class="card-body">
-            <!--manager-->
-            <h4 class="manager-title">${myTeam.members[0].name}</h4>
-            <p class = "manager-icon">${myTeam.members[0].position}</p>
-            <p class="card-text">ID: <span class="manager-id">${myTeam.members[0].id}</span></p>
-            <p class="card-text"><span class="manager-email">Email: </span><a href="mailto:${myTeam.members[0].email}">${myTeam.members[0].email}</a></p>
-            <p class="card-text">Office Number: <span class ="manager-office">${myTeam.members[0].officeNumber}</span></p>
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card">
+            <div class="card-body">
+              <!--manager-->
+              <h4 class="manager-title">${myTeam.members[0].name}</h4>
+              <p class = "manager-icon">${myTeam.members[0].position}</p>
+              <p class="card-text">ID: <span class="manager-id">${myTeam.members[0].id}</span></p>
+              <p class="card-text"><span class="manager-email">Email: </span><a href="mailto:${myTeam.members[0].email}">${myTeam.members[0].email}</a></p>
+              <p class="card-text">Office Number: <span class ="manager-office">${myTeam.members[0].officeNumber}</span></p>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <!--engineers & interns-->
-      ${renderEngineers(myTeam)}
-    
-      ${renderInterns(myTeam)}
+        <!--engineers & interns-->
+        ${renderEngineers(myTeam)}
       
+        ${renderInterns(myTeam)}
+    
+      </div>
     </div>
 
     <!--footer-->
